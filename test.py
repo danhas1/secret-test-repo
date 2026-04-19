77c233b08fd932913ba6347409540e459ad41654
DanHason <danhason37@gmail.com>
2026-04-19 08:56:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -23,23 +23,18 @@ class UserService:
     def add_user(self, name):
         self.users.append(name)
 
-# --- פה "טעות אמיתית" ---
 
 password = "12345678"
 
-# עוד קוד רגיל
 def connect():
     print("Connecting to DB...")
 
 # API config
 api_url = "https://api.example.com"
 
-# --- עוד טעות ---
 api_key = "ABCDEFG1234567890ABCDEFG"
 
-# עוד קוד
 def run():
     print("App running")
 
-# --- טעות מוסתרת ---
 aws_key = "AKIA1234567890ABCDEF"
